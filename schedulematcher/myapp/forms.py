9d32736e9685f8343071fcb12b5c733b35b6f9c4
Xyc330 <yichengxia.xyc@gmail.com>
2026-01-17 08:38:03 -0500

    Broken code

---
+++ schedulematcher/myapp/forms.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+
+CustomUser=get_user_model()
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        fields = UserCreationForm.Meta.fields
--- schedulematcher/myapp/views.py
+++ schedulematcher/myapp/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth import authenticate, login, logout, get_user_model
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 from myapp.parser.parser import OmnivoxScheduleParser
 from myapp.models import Block, Day,Schedule,Team,User
 from django.core.files.storage import default_storage
@@ -6,61 +9,132 @@ import os
 import math
 import json
 from datetime import datetime, timedelta
-# Create your views here.
+from .forms import CustomUserCreationForm
+User = get_user_model()
 
 def welcomepage(request):
-    return render(request, "welcomepage.html")
+    if request.method == "POST":
+        pass
+    else:
+        return render(request, "welcomepage.html", {})
+
+    
+    
 
 def home(request):
     events = []
     if request.method == "POST":
+      
         try:
-            pdf = request.FILES['pdfFile']
+            email = request.POST["email"]
+            password = request.POST["password"]
+
         except:
-            print("No pdf")
-            return render(request, 'home.html', {'events': events})
+            return render(request, 'home.html')
+        
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.success(request, ("Denied"))
+            return redirect('welcomepage')
+
+        
            
 
-        schedule = convert(pdf) # List of 5 lists each containing blocks
+        # schedule = convert(pdf) # List of 5 lists each containing blocks
         
-        weekday_map = {0: 0, 1: 1, 2: 2, 3: 3, 4: 4} 
-        start_date = datetime(2026, 1, 5)
+        # weekday_map = {0: 0, 1: 1, 2: 2, 3: 3, 4: 4} 
+        # start_date = datetime(2026, 1, 5)
 
-        for week in range(4):
-            for day_index, day_blocks in enumerate(schedule):
-                for block in day_blocks:
-                    # Get block date
-                    block_date = start_date + timedelta(days=weekday_map[day_index] + week*7)
+        # for week in range(4):
+        #     for day_index, day_blocks in enumerate(schedule):
+        #         for block in day_blocks:
+        #             # Get block date
+        #             block_date = start_date + timedelta(days=weekday_map[day_index] + week*7)
 
-                    # Convert integer times to "HH:MM" string
-                    start_str = intToTime(block.startTime)
-                    end_str = intToTime(block.endTime)
+        #             # Convert integer times to "HH:MM" string
+        #             start_str = intToTime(block.startTime)
+        #             end_str = intToTime(block.endTime)
 
-                    # Combine with block date to make ISO datetime
-                    start_dt = datetime.fromisoformat(f"{block_date.date()}T{start_str}")
-                    end_dt = datetime.fromisoformat(f"{block_date.date()}T{end_str}")
+        #             # Combine with block date to make ISO datetime
+        #             start_dt = datetime.fromisoformat(f"{block_date.date()}T{start_str}")
+        #             end_dt = datetime.fromisoformat(f"{block_date.date()}T{end_str}")
 
-                    events.append({
-                        'title': block.name,
-                        'start': start_dt.isoformat(),
-                        'end': end_dt.isoformat(),
-                        'color': '#007EA7'
-                    })
+        #             events.append({
+        #                 'title': block.name,
+        #                 'start': start_dt.isoformat(),
+        #                 'end': end_dt.isoformat(),
+        #                 'color': '#007EA7'
+        #             })
 
 
-        for i in range(len(schedule)):
-            for j in range (len(schedule[i])):
-                block = schedule[i][j]
-                print(i)
-                print(block.name)
-                print(block.startTime)
-                print(block.endTime)
+        # for i in range(len(schedule)):
+        #     for j in range (len(schedule[i])):
+        #         block = schedule[i][j]
+        #         print(i)
+        #         print(block.name)
+        #         print(block.startTime)
+        #         print(block.endTime)
                       
 
     return render(request, 'home.html', {'events': events})
 
 def createaccount(request):
-    return render(request, "createaccount.html")
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        print(request)
+        if form.is_valid():
+            form.save()
+            
+            try:
+                pdf = request.FILES['pdfFile']
+            except:
+                return redirect("welcomepage")
+            
+            schedule = convert(pdf) # List of 5 lists each containing blocks
+
+            weekday_map = {0: 0, 1: 1, 2: 2, 3: 3, 4: 4} 
+            start_date = datetime(2026, 1, 5)
+
+            for week in range(4):
+                for day_index, day_blocks in enumerate(schedule):
+                    for block in day_blocks:
+                        # Get block date
+                        block_date = start_date + timedelta(days=weekday_map[day_index] + week*7)
+
+                        # Convert integer times to "HH:MM" string
+                        start_str = intToTime(block.startTime)
+                        end_str = intToTime(block.endTime)
+
+                        # Combine with block date to make ISO datetime
+                        start_dt = datetime.fromisoformat(f"{block_date.date()}T{start_str}")
+                        end_dt = datetime.fromisoformat(f"{block_date.date()}T{end_str}")
+
+                        events.append({
+                            'title': block.name,
+                            'start': start_dt.isoformat(),
+                            'end': end_dt.isoformat(),
+                            'color': '#007EA7'
+                        })
+
+
+            for i in range(len(schedule)):
+                for j in range (len(schedule[i])):
+                    block = schedule[i][j]
+                    print(i)
+                    print(block.name)
+                    print(block.startTime)
+                    print(block.endTime)
+
+            return render(request, 'home.html', {'events': events})
+        
+        
+    else:
+        form = UserCreationForm()
+
+    return render(request, "createaccount.html", {"form": form})
 
 def creategroup(request):
     return render(request, "creategroup.html")
