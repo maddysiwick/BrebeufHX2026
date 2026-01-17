cb0e7d0e01d2f150f50f114a4958da533b26b7fb
Xyc330 <yichengxia.xyc@gmail.com>
2026-01-16 21:51:38 -0500

    Day

--- schedulematcher/myapp/models.py
+++ schedulematcher/myapp/models.py
@@ -4,11 +4,18 @@ from django.contrib.auth import AbstractUser
 class User(AbstractUser):
     pass
 
+class Day(models.Model):
+    pass
+
 class Block(models.Model):
     name = models.CharField(max_length=100)
+    day = models.ForeignKey(Day, on_delete=models.CASCADE)
     start_time = models.IntegerField()
     end_time = models.IntegerField()
     mandatory = models.BooleanField()
+
+
+
 
 
 
