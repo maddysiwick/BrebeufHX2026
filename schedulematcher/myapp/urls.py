72ed67065c527eb343cb0336d32416caa7bd5d3c
EvaBatal9 <eva.batal2008@gmail.com>
2026-01-16 23:09:39 -0500

    url fixes

--- schedulematcher/myapp/urls.py
+++ schedulematcher/myapp/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path("", views.welcomepage, name="welcomepage"),
     path("home/", views.home, name="home"),
     path("createaccount/", views.createaccount, name="createaccount"),
+<<<<<<< HEAD
     path("login/", views.createaccount, name="login"),
+=======
+>>>>>>> 1dfd7d2 (url fixes)
 ]
