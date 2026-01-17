bbb9a3397225c5d601b95154c6f09438579f2624
PureLumpyAttack <PureLumpyAttack@users.noreply.github.com>
2026-01-17 01:24:54 -0500

    feat: uhhhh improve? idk

--- schedulematcher/myapp/urls.py
+++ schedulematcher/myapp/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path("createaccount/", views.createaccount, name="createaccount"),
     path("login/", views.createaccount, name="login"),
     path("creategroup/", views.creategroup, name="creategroup"),
-    path("match/", views.match, name="match"),
+    #path("match/", views.match, name="match"),
 ]
