1692a4643df865f63f523c5c72ff3b969e691032
Xyc330 <yichengxia.xyc@gmail.com>
2026-01-16 21:44:05 -0500

    User and Block models

--- schedulematcher/myapp/models.py
+++ schedulematcher/myapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    pass
+
+class Block(models.Model):
+    name = models.CharField(max_length=100)
+    start_time = models.IntegerField()
+    end_time = models.IntegerField()
+    mandatory = models.BooleanField()
+
+
+
+
+
+
+
+
+
+
+
+
